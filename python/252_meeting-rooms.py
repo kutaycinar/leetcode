4750c0e8bf8495a60e916c64da161c814e68a4ab
Kutay Cinar <gmail@kutaycinar.com>
2022-11-21 16:53:11 +0300

    21, 252 and 338 python

---
+++ python/252_meeting-rooms.py
@@ -0,0 +1,38 @@
+from typing import List
+
+
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def __repr__(self):
+        return f"({self.start}, {self.end})"
+
+
+class Solution:
+    """
+    @param intervals: an array of meeting time intervals
+    @return: if a person could attend all meetings
+    """
+
+    def can_attend_meetings(self, intervals: List[Interval]) -> bool:
+
+        intervals.sort(key=lambda i: (i.start, i.end))
+        print(intervals)
+
+        prev = intervals[0].end
+
+        for i in intervals[1:]:
+
+            if i.start < prev:
+                print(i.start, prev)
+                return False
+            else:
+                prev = i.end
+
+        return True
+
+
+# Time:  O(nlogn)
+# Space: O(1)
---
+++ python/338_counting-bits.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = [0] * (n + 1)
+        for i in range(1, n + 1):
+            ans[i] = ans[i // 2] + i % 2
+        print(ans)
+
+
+# Time:  O(nlogn)
+# Space: O(n)
