6556e5a1a00523a73f755491b0e62f0ca655cf98
Kutay Cinar <gmail@kutaycinar.com>
2022-11-24 18:06:40 +0300

    delete ts

--- ts/1410_html-entity-parser.ts
+++
@@ -1,9 +0,0 @@
-function entityParser(text: string): string {
-    return text
-        .replace(/&gt;/g, `>`)
-        .replace(/&lt;/g, `<`)
-        .replace(/&frasl;/g, `/`)
-        .replace(/&quot;/g, `"`)
-        .replace(/&apos;/g, `'`)
-        .replace(/&amp;/g, `&`)
-};
