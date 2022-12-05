ce61298de65e92a9e22ffcb0d7aae644d0e7113a
Kutay Cinar <gmail@kutaycinar.com>
2022-12-05 16:02:13 +0300

    top k frequent elements py

---
+++ python/347_top-k-frequent-elements.py
@@ -0,0 +1,24 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+
+        freq = [[] for _ in range(len(nums)+1)]
+        for key, value in count.items():
+            freq[value].append(key)
+
+        res = []
+        for i in range(len(freq)-1, -1, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+
+
+# Time:  O(n)
+# Space: O(n)
