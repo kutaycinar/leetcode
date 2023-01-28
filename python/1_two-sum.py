b73b8784502f9edd36a7535f4a443228b37d600b
Kutay Cinar <gmail@kutaycinar.com>
2023-01-29 01:46:30 +0300

    refactor

--- python/1_two-sum.py
+++ python/1_two-sum.py
@@ -4,6 +4,13 @@ from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
+        print("hello")
+        a = nums[0]
+        b = nums[2]
+        print(a + b)
+
+        return None
+
         hash = {}
 
         for i, n in enumerate(nums):
@@ -14,3 +21,5 @@ class Solution:
 
 # Time:  O(n)
 # Space: O(n)
+
+Solution().twoSum([1,2,4,5], 7)
