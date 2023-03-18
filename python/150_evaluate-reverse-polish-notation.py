b325425e4e037f961ab907ddfbc40345e2a52472
Kutay Cinar <76612427+kutaycinar@users.noreply.github.com>
2023-03-19 00:45:45 +0300

    lambda functions

--- python/150_evaluate-reverse-polish-notation.py
+++ python/150_evaluate-reverse-polish-notation.py
@@ -3,24 +3,22 @@ from typing import List
 
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        operands = ['+', '-', '*', '/']
+        operands = {
+            '+': lambda a, b: a + b,
+            '-': lambda a, b: a - b,
+            '*': lambda a, b: a * b,
+            '/': lambda a, b: a / b
+        }
         stack = []
         for t in tokens:
             if t in operands:
                 b, a = stack.pop(), stack.pop()
-                match t:
-                    case '+':
-                        stack.append(a + b)
-                    case '-':
-                        stack.append(a - b)
-                    case '*':
-                        stack.append(a * b)
-                    case '/':
-                        stack.append(int(a / b))
+                res = operands[t](a, b)
+                stack.append(res)
             else:
                 stack.append(int(t))
 
         return stack.pop()
-    
 
-Solution().evalRPN(tokens=["2", "1", "+", "3", "*"])
+# Time:  O(n)
+# Space: O(n)
